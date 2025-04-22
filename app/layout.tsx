7b7ac72b14bfa929cbafc12a868c99dcecedb15f
idkwhttsay <danil.tatarinov.00@gmail.com>
2025-04-22 16:11:01 -0400

    feat: add more metadata and manifest.json

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,6 +14,43 @@ export const metadata: Metadata = {
         shortcut: '/icon.png',
         apple: '/icon.png',
     },
+    keywords: [
+        'Central Asian Student Association',
+        'Georgia Tech',
+        'GT',
+        'CASA',
+        'Central Asia',
+        'Student Organization',
+        'Student Association',
+    ],
+    metadataBase: new URL('https://casa.tatarinov.org'),
+    openGraph: {
+        title: 'CASA at Georgia Tech',
+        description: 'Connect with the Central Asian community at Georgia Tech',
+        images: [
+            {
+                url: '/icon.png',
+                width: 1200,
+                height: 630,
+                alt: 'CASA GT Logo',
+            },
+        ],
+        locale: 'en_US',
+        type: 'website',
+    },
+    robots: {
+        index: true,
+        follow: true,
+    },
+    alternates: {
+        canonical: '/',
+        languages: {
+            'en-US': '/en-US',
+        },
+    },
+    authors: [{ name: 'CASA GT' }],
+    category: 'Student Organization',
+    manifest: '/manifest.json',
 };
 
 export default function RootLayout({
